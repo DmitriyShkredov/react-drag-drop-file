5db969049a54d252abda50b21435273e452ea897
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2023-08-05 10:42:32 +0300

    feat: add grag drop form

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,90 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [files, setFiles] = useState([]);
+  const [dragActive, setDragActive] = useState(false);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    if (e.target.files && e.target.files[0]) {
+      setFiles([...e.target.files]);
+    }
+  };
+
+  const handleReset = () => {
+    setFiles([]);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = new FormData();
+    files.forEach((file) => {
+      data.append("file", file);
+    });
+    fetch("https://someapi", { method: "POST", body: data })
+      .then((response) => response.json())
+      .then(() => setFiles([]))
+      .catch(() => setFiles([]));
+  };
+
+  const handleDrag = function (e) {
+    e.preventDefault();
+    setDragActive(true);
+  };
+
+  const handleLive = function (e) {
+    e.preventDefault();
+    setDragActive(false);
+  };
+
+  const handleDrop = function (e) {
+    e.preventDefault();
+    setDragActive(false);
+    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+      setFiles([...e.dataTransfer.files]);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Типичный веб разработчик</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Drag and Drop загрузка файлов перетаскиванием
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Drag & Drop</h1>
+      <form
+        className={`form ${dragActive ? "drag" : ""}`}
+        onReset={handleReset}
+        onSubmit={handleSubmit}
+        onDragEnter={handleDrag}
+        onDragOver={handleDrag}
+        onDragLeave={handleLive}
+        onDrop={handleDrop}
+      >
+        <h2>Перетащите файлы сюда</h2>
+        <p>или</p>
+        <label className="label">
+          <span>Загрузите файл</span>
+          <input
+            className="input"
+            type="file"
+            multiple={true}
+            onChange={handleChange}
+          />
+        </label>
+        {files.length > 0 && (
+          <>
+            <ul className="file-list">
+              {files.map(({ name }, id) => (
+                <li key={id}>{name}</li>
+              ))}
+            </ul>
+            <button className="button-reset" type="reset">
+              Отменить
+            </button>
+            <button className="button-submit" type="submit">
+              Отправить
+            </button>
+          </>
+        )}
+      </form>
     </div>
   );
 }
